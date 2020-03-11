73d75fec52df76a33f3374f51d1c5aeafbb0a376
Your Name <hungvv3@cenland.com.vn>
2020-03-11 23:09:29 +0700

    update

---
+++ events-processor/app.py
@@ -0,0 +1,50 @@
+from confluent_kafka import Producer, Consumer, KafkaError
+import snowplow_analytics_sdk.event_transformer
+import snowplow_analytics_sdk.snowplow_event_transformation_exception
+import json
+
+def getConfig():
+    with open('./config.json') as config_file:
+        config = json.load(config_file)
+    return config
+
+if __name__=="__main__":
+
+    config = getConfig()
+
+    kafka_consumer = Consumer({
+        'bootstrap.servers': config['bootstrap_servers'],
+        'group.id': config['group_id'],
+        'default.topic.config': {
+            'auto.offset.reset': config['offset']
+        }
+    })
+
+    kafka_producer = Producer({
+        'bootstrap.servers': config['bootstrap_servers'],
+    })
+
+    kafka_consumer.subscribe(['snowplow_enriched_good'])
+
+    while True:
+        msg = kafka_consumer.poll(1.0)
+
+        if msg is None:
+            continue
+        if msg.error():
+            if msg.error().code() == KafkaError._PARTITION_EOF: continue
+            else:
+                print("kafka error: " + msg.error())
+                break
+
+        event = msg.value().decode('utf-8')
+        # print(event)
+        try:
+            json_data = snowplow_analytics_sdk.event_transformer.transform(event)
+            kafka_producer.poll(0)
+            kafka_producer.produce('snowplow_json_event', json.dumps(json_data).encode('utf-8'))
+            kafka_producer.flush()
+
+        except snowplow_analytics_sdk.snowplow_event_transformation_exception.SnowplowEventTransformationException as e:
+            for error_message in e.error_messages:
+                print("kafka error: " + error_message)
---
+++ events-to-es/app.py
@@ -0,0 +1,50 @@
+from kafka import KafkaConsumer
+import traceback
+import json
+from elasticsearch import Elasticsearch, helpers
+
+def getConfig():
+    with open('./config.json') as config_file:
+        config = json.load(config_file)
+    return config
+
+def get_client():
+    config = getConfig()
+    return Elasticsearch([config['es_port']])
+
+def kafka_to_es(record):
+    es_record = {'_index': 'test_log_snowplow',
+                   '_type': '_doc',
+                   '_id': record['etl_tstamp'] + '_' + record['event_name'],
+                   '_source': record}
+    return es_record
+
+def push_kafka_to_es(es_records):
+    es_client = get_client()
+    helpers.bulk(es_client, es_records, chunk_size=500, request_timeout=20)
+
+if __name__=="__main__":
+    config = getConfig()
+
+    kafka_consumer = KafkaConsumer(
+        group_id=config['group_id'],
+        bootstrap_servers=config['bootstrap_servers'],
+        auto_offset_reset=config['offset'],
+        value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+    )
+    kafka_consumer.subscribe(['snowplow_json_event'])
+
+    es_records = []
+
+    while True:
+        message = next(kafka_consumer)
+        if message is None: continue
+        json_event = message.value
+        print(json_event)
+        try:
+            es_records.append(kafka_to_es(json_event))
+            if(len(es_records) == 10 ):
+                push_kafka_to_es(es_records)
+                es_records.clear()
+        except Exception as e:
+            traceback.print_exc()
